88864b01bfd6a0279925817174d7b7677ca3afaa
798941103 <798941103@qq.com>
2019-10-31 21:17:48 +0800

    添加项目文件。

---
+++ Assets/Pacmanmove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacmanmove : MonoBehaviour
+{
+    public float speed = 0.3f;
+    private Vector2 dest = Vector2.zero;
+    private void Start()
+    {
+        dest = transform.position;
+    }
+    private void FixedUpdate()
+    {
+        Vector2 temp = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(temp);
+        if ((Vector2)transform.position==dest)
+        {
+            if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))&&Valid(Vector2.up))
+            {
+                dest = (Vector2)transform.position + Vector2.up;
+            }
+            if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && Valid(Vector2.down))
+            {
+                dest = (Vector2)transform.position + Vector2.down;
+            }
+            if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && Valid(Vector2.right))
+            {
+                dest = (Vector2)transform.position + Vector2.right;
+            }
+            if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && Valid(Vector2.left))
+            {
+                dest = (Vector2)transform.position + Vector2.left;
+            }
+        }
+    }
+    private bool Valid(Vector2 dir)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
